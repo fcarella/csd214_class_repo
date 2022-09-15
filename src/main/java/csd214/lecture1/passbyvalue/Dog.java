7d868b40031641aa3b612aeb438465d31e60fd8f
Fred Carella <fred.carella@gmail.com>
2022-09-15 09:19:24 -0400

    commit

---
+++ src/main/java/csd214/lecture1/passbyvalue/Dog.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csd214.lecture1.passbyvalue;
+
+/**
+ *
+ * @author students
+ */
+class Dog {
+
+    private String name;
+
+    Dog(String name) {
+        this.name=name;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
---
+++ src/main/java/csd214/lecture1/passbyvalue/Main.java
@@ -0,0 +1,44 @@
+/*
+ * See https://stackoverflow.com/questions/40480/is-java-pass-by-reference-or-pass-by-value
+ */
+package csd214.lecture1.passbyvalue;
+
+/**
+ *
+ * @author students
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // run this in the debugger to see what's going on.
+
+        Dog aDog = new Dog("Max");
+        Dog oldDog = aDog;
+        oldDog.setName("George");
+        aDog.setName("Max");
+        //   aDog --> [dog object]
+        // oldDog --> aDog
+
+        // we pass the object to foo
+        // java passes by value, so here, we pass a COPY of the reference to foo, 
+        // not the object and not the original reference itself
+        foo(aDog);
+        // aDog variable is still pointing to the original "Max" dog when foo(...) returns
+        aDog.getName().equals("Max"); // true
+        aDog.getName().equals("Fifi"); // false
+        System.out.println("aDog == oldDog is "+ (aDog == oldDog)); // true
+    }
+
+    public static void foo(Dog d) {
+        d.setName("Jill");
+        // although d refers to the original object, d is actually a copy of the 
+        // original reference.  when we modify what d points to below,
+        // we modify the COPY not he original.  So the original reference
+        // still points to the original dog.
+        System.out.println("d.getName().equals(\"Max\") is "+d.getName().equals("Max")); // true
+        // change d inside of foo() to point to a new Dog instance "Fifi"
+        d = new Dog("Fifi");
+        System.out.println("d.getName().equals(\"Fifi\") is "+d.getName().equals("Fifi")); // true
+    }
+}

1f455cc0c271119eebf44f959d890ee7fa791639
Fred Carella <fred.carella@gmail.com>
2022-09-20 11:32:29 -0400

    commit

--- src/main/java/csd214/lecture1/passbyvalue/Main.java
+++ src/main/java/csd214/lecture1/passbyvalue/Main.java
@@ -33,13 +33,13 @@ public class Main {
     public static void foo(Dog d) {
         d.setName("Jill");
         System.out.println("d.getName().equals(\"Max\") is "+d.getName().equals("Max")); // true
+        d = new Dog("Fifi");
         // change d inside of foo() to point to a new Dog instance "Fifi"
         // NOTE ** on method entry, d refers to the original object.  But d is actually a copy of the
         // original reference so when we modify what d points to below,
         // we modify the COPY not he original.  So the original reference
         // still points to the original dog.
         // So d is Fifi in this method but the original object is still Jill
-        d = new Dog("Fifi");
         System.out.println("d.getName().equals(\"Fifi\") is "+d.getName().equals("Fifi")); // true
     }
 }

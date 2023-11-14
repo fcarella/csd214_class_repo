6a6e0f1513d5bc47db27f76688c8a90798c63a87
Fred <fred.carella@gmail.com>
2023-11-14 10:48:50 -0500

    commit

---
+++ src/main/java/csd214/textbook/BJ7_HTML_LectureSlides/code/ch12/section_2/ChoiceQuestion.java
@@ -0,0 +1,48 @@
+package csd214.textbook.BJ7_HTML_LectureSlides.code.ch12.section_2;
+
+import java.util.ArrayList;
+
+/**
+   A question with multiple choices.
+*/
+public class ChoiceQuestion extends Question
+{
+   private ArrayList<String> choices;
+
+   /**
+      Constructs a choice question with no choices.
+   */
+   public ChoiceQuestion()
+   {
+      choices = new ArrayList<String>();
+   }
+
+   /**
+      Adds an answer choice to this question.
+      @param choice the choice to add
+      @param correct true if this is the correct choice, false otherwise
+   */
+   public void addChoice(String choice, boolean correct)
+   {
+      choices.add(choice);
+      if (correct) 
+      {
+         // Convert choices.size() to string
+         String choiceString = "" + choices.size();
+         setAnswer(choiceString);
+      }
+   }
+   
+   public void display()
+   {
+      // Display the question text
+      super.display();
+      // Display the answer choices
+      for (int i = 0; i < choices.size(); i++)
+      {
+         int choiceNumber = i + 1;
+         System.out.println(choiceNumber + ": " + choices.get(i));     
+      }
+   }
+}
+
